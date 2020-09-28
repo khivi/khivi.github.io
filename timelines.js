7de1ea65edd0a7a54b587c01f29af5a9ac153624
khivi <work@khivi.com>
2020-09-28 14:32:05 -0400

    Updated Website on Mon Sep 28 14:32:05 EDT 2020

--- timelines.js
+++ timelines.js
@@ -1,46 +1,48 @@
+/* eslint space-before-function-paren: 0 */
+/* eslint semi: 2 */
+
 var global = window; // https://github.com/marmelab/EventDrops/issues/258
 
 function drawGraph() {
-  Date.prototype.diffDays = function(date) { 
-      const timeDiff = Math.abs(date.getTime() - this.getTime());
-      const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-      return diffDays;
-  }
+  Date.prototype.diffDays = function(date) {
+    const timeDiff = Math.abs(date.getTime() - this.getTime());
+    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return diffDays;
+  };
 
-  Date.prototype.addDays = function(days)
-  {
-      const dat = new Date(this.valueOf());
-      dat.setDate(dat.getDate() + days);
-      return dat;
-  }
+  Date.prototype.addDays = function(days) {
+    const dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+  };
 
   Date.prototype.upto = function(endDate) {
-      var d;
-      var date;
-      var dates = [];
-      const start = new Date(this.getFullYear(), 1, 1);
-      const end = new Date(endDate.getFullYear(), 12, 31);
-      var days = start.diffDays(end);
-      for (d = 0; d < days; d = d + 365) { 
-          date = start.addDays(d);
-          dates.push(date);
-      }
-      return dates;
-  } 
+    var d;
+    var date;
+    var dates = [];
+    const start = new Date(this.getFullYear(), 1, 1);
+    const end = new Date(endDate.getFullYear(), 12, 31);
+    var days = start.diffDays(end);
+    for (d = 0; d < days; d = d + 365) {
+      date = start.addDays(d);
+      dates.push(date);
+    }
+    return dates;
+  };
 
   const concatDates = function() {
-      var combined = arguments[0];
-      for (var i = 1; i < arguments.length; i++) {
-        combined = combined.concat(arguments[i]);
-      }
-      return combined;
-  }
+    var combined = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+      combined = combined.concat(arguments[i]);
+    }
+    return combined;
+  };
 
   var data = {};
   var ranges = {};
   var companies = {};
 
-  const start = new Date(1985,1,1);
+  const start = new Date(1985, 1, 1);
   const today = new Date();
   const today1Year = new Date().setFullYear(today.getFullYear() + 1);
 
@@ -57,21 +59,20 @@ function drawGraph() {
   companies.needl = new Date(2019, 3, 1).upto(today);
   companies.innovision = new Date(2020, 6, 1).upto(today);
   data.companies = [
-      { name: 'Bell Labs ', dates: companies.belllabs },
-      { name: 'Xebeo ', dates: companies.xebeo },
-      { name: 'Roundbox ', dates: companies.roundbox },
-      { name: 'Meshcapital ', dates: companies.mesh },
-      { name: 'Konnect2  ', dates: companies.konnect2 },
-      { name: 'Betterpath ', dates: companies.betterpath },
-      { name: 'Emissary ', dates: companies.emissary },
-      { name: 'Target ', dates: companies.target },
+    { name: 'Bell Labs ', dates: companies.belllabs },
+    { name: 'Xebeo ', dates: companies.xebeo },
+    { name: 'Roundbox ', dates: companies.roundbox },
+    { name: 'Meshcapital ', dates: companies.mesh },
+    { name: 'Konnect2  ', dates: companies.konnect2 },
+    { name: 'Betterpath ', dates: companies.betterpath },
+    { name: 'Emissary ', dates: companies.emissary },
+    { name: 'Target ', dates: companies.target }
   ];
-
 
   ranges.lang = {};
   ranges.lang.basic = new Date(1987, 7, 1).upto(new Date(1990, 1, 1));
-  //ranges.lang.fortran = new Date(1990, 7, 1).upto(new Date(1990, 12, 1));
-  //ranges.lang.pascal = new Date(1991, 7, 1).upto(new Date(1992, 7, 1));
+  // ranges.lang.fortran = new Date(1990, 7, 1).upto(new Date(1990, 12, 1));
+  // ranges.lang.pascal = new Date(1991, 7, 1).upto(new Date(1992, 7, 1));
   ranges.lang.cschool = new Date(1992, 7, 1).upto(new Date(1996, 7, 1));
   ranges.lang.pythonschool = new Date(1992, 5, 1).upto(new Date(1994, 7, 1));
 
@@ -84,14 +85,14 @@ function drawGraph() {
   ranges.lang.javascript = concatDates(companies.konnect2, companies.needl);
   ranges.lang.python = concatDates(ranges.lang.pythonschool, companies.xebeo, companies.emissary, companies.target, companies.needl);
   data.lang = [
-      { name: 'Basic ', dates: ranges.lang.basic },
-      { name: 'C/C++', dates: concatDates(ranges.lang.c, ranges.lang.cp) },
-      { name: 'Java', dates: ranges.lang.java },
-      { name: 'Python', dates: ranges.lang.python },
-      { name: 'Objective-C', dates: ranges.lang.co },
-      { name: 'php', dates: ranges.lang.php },
-      { name: 'Javascript', dates: ranges.lang.javascript },
-      { name: 'Scala', dates: ranges.lang.scala },
+    { name: 'Basic ', dates: ranges.lang.basic },
+    { name: 'C/C++', dates: concatDates(ranges.lang.c, ranges.lang.cp) },
+    { name: 'Java', dates: ranges.lang.java },
+    { name: 'Python', dates: ranges.lang.python },
+    { name: 'Objective-C', dates: ranges.lang.co },
+    { name: 'php', dates: ranges.lang.php },
+    { name: 'Javascript', dates: ranges.lang.javascript },
+    { name: 'Scala', dates: ranges.lang.scala }
   ];
 
   ranges.script = {};
@@ -102,17 +103,17 @@ function drawGraph() {
   ranges.script.ansible = concatDates(companies.konnect2, companies.betterpath, companies.verne, companies.target);
   ranges.script.docker = concatDates(companies.betterpath, companies.verne, companies.emissary, companies.target, companies.needl);
   ranges.script.k8s = concatDates(companies.target, companies.innovision);
-  ranges.script.gcp = concatDates(companies.emissary)
-  ranges.script.aws = concatDates(companies.konnect2, companies.needl, companies.innovision)
+  ranges.script.gcp = concatDates(companies.emissary);
+  ranges.script.aws = concatDates(companies.konnect2, companies.needl, companies.innovision);
   data.script = [
-      { name: 'Ansible ', dates: ranges.script.ansible },
-      { name: 'Bash ', dates: ranges.script.bash },
-      { name: 'Perl ', dates: ranges.script.perl },
-      { name: 'Ruby ', dates: ranges.script.ruby },
-      { name: 'Docker ', dates: ranges.script.docker },
-      { name: 'Kubernetes ', dates: ranges.script.k8s },
-      { name: 'Google Cloud ', dates: ranges.script.gcp },
-      { name: 'AWS ', dates: ranges.script.aws },
+    { name: 'Ansible ', dates: ranges.script.ansible },
+    { name: 'Bash ', dates: ranges.script.bash },
+    { name: 'Perl ', dates: ranges.script.perl },
+    { name: 'Ruby ', dates: ranges.script.ruby },
+    { name: 'Docker ', dates: ranges.script.docker },
+    { name: 'Kubernetes ', dates: ranges.script.k8s },
+    { name: 'Google GCP', dates: ranges.script.gcp },
+    { name: 'AWS ', dates: ranges.script.aws }
   ];
 
   ranges.tools = {};
@@ -126,15 +127,15 @@ function drawGraph() {
   ranges.tools.jira = concatDates(companies.emissary, companies.target);
   ranges.tools.basecamp = concatDates(companies.verne);
   data.tools = [
-      { name: 'Asana ', dates: ranges.tools.asana },
-      { name: 'Assembla ', dates: ranges.tools.assembla },
-      { name: 'Bugzilla ', dates: ranges.tools.bugzilla },
-      { name: 'GitHub ', dates: ranges.tools.github },
-      { name: 'Jira ', dates: ranges.tools.jira },
-      { name: 'Basecamp ', dates: ranges.tools.basecamp },
-      { name: 'Perforce ', dates: ranges.tools.perforce },
-      { name: 'Sablime ', dates: ranges.tools.sablime },
-      { name: 'Trello ', dates: ranges.tools.trello }
+    { name: 'Asana ', dates: ranges.tools.asana },
+    { name: 'Assembla ', dates: ranges.tools.assembla },
+    { name: 'Bugzilla ', dates: ranges.tools.bugzilla },
+    { name: 'GitHub ', dates: ranges.tools.github },
+    { name: 'Jira ', dates: ranges.tools.jira },
+    { name: 'Basecamp ', dates: ranges.tools.basecamp },
+    { name: 'Perforce ', dates: ranges.tools.perforce },
+    { name: 'Sablime ', dates: ranges.tools.sablime },
+    { name: 'Trello ', dates: ranges.tools.trello }
   ];
 
   ranges.os = {};
@@ -147,77 +148,74 @@ function drawGraph() {
   ranges.os.ios = new Date(2008, 7, 1).upto(new Date(2010, 7, 1));
   ranges.os.android = new Date(2016, 1, 1).upto(new Date(2016, 6, 1));
   data.os = [
-      { name: 'Android ', dates: ranges.os.android },
-      { name: 'FreeBSD ', dates: ranges.os.freebsd },
-      { name: 'IOS ', dates: ranges.os.ios },
-      { name: 'Linux ', dates: ranges.os.linux },
-      { name: 'OS X ', dates: ranges.os.osx },
-      { name: 'Solaris ', dates: ranges.os.solaris },
-      { name: 'Unix System V ', dates: ranges.os.systemV },
-      { name: 'ZX Spectrum ', dates: ranges.os.spectrum }
+    { name: 'Android ', dates: ranges.os.android },
+    { name: 'FreeBSD ', dates: ranges.os.freebsd },
+    { name: 'IOS ', dates: ranges.os.ios },
+    { name: 'Linux ', dates: ranges.os.linux },
+    { name: 'OS X ', dates: ranges.os.osx },
+    { name: 'Solaris ', dates: ranges.os.solaris },
+    { name: 'Unix SVR ', dates: ranges.os.systemV },
+    { name: 'ZX Sinclair ', dates: ranges.os.spectrum }
   ];
 
   const content = document.getElementById('content');
 
-  const getChart = function() { 
-      const color = d3.scaleOrdinal(d3.schemeAccent);
-      const chart = eventDrops({
-        d3,
-        drop: {
-          date: d => new Date(d.date),
-        },
-        zoom: false,
-        range: {
-          start: start,
-          end: today1Year,
-        },
-        axis: {
-          formats: {
-            year: '%y',
-          },
-        },
-        bound: { 
-          format: d3.timeFormat('%Y'),
-        },
-        label: {
-          text: d => `${d.name} (${d.data.length} years)`,
-        },
-      });
-      return chart;
+  const getChart = function() {
+    return eventDrops({
+      d3,
+      drop: {
+        date: d => new Date(d.date)
+      },
+      zoom: false,
+      range: {
+        start: start,
+        end: today1Year
+      },
+      axis: {
+        formats: {
+          year: '%y'
+        }
+      },
+      bound: {
+        format: d3.timeFormat('%Y')
+      },
+      label: {
+        text: d => `${d.name} (${d.data.length}y)`
+      }
+    });
+  };
+
+  const setData = function(name, data) {
+    const chartData = data.map(d => ({
+      name: d.name,
+      data: d.dates.map(date => ({ date: date }))
+    }));
+    const chart = getChart();
+    d3.select(content)
+      .append('div')
+      .append('h2')
+      .html(name)
+      .append('div')
+      .attr('id', name);
+
+    const div = document.getElementById(name);
+    d3.select(div)
+      .data([chartData])
+      .call(chart);
   }
 
-  const setData = function(name, data) { 
-      const chartData = data.map(d => ({
-        name: d.name,
-        data: d.dates.map(date => ({"date": date})),
-      }));
-      const chart = getChart();
-      d3.select(content)
-		.append('div')
-        .append('h2')
-        .html(name)
-        .append('div')
-        .attr('id', name);
-
-      const div = document.getElementById(name);
-      d3.select(div)
-    	.data([chartData])
-    	.call(chart);
-  }
-
-  //setData("Companies", data.companies);
+  // setData("Companies", data.companies);
   setData('Operating Systems', data.os);
   setData('Programming Languages', data.lang);
   setData('DevOps', data.script);
   setData('Product', data.tools);
-  //d3.select(content).selectAll('svg g.extremum').remove();
-  //d3.select(content).selectAll('svg').attr('width', 800);
-  //d3.select(content).selectAll('svg').attr('transform', 'translate(-90, 0)');
-  d3.select(content).selectAll('svg g.labels').attr('transform', 'translate(-90, 45)');
-  //d3.select(content).selectAll('svg g.axes').attr('transform', 'translate(120, 55)'); 
-  //d3.select(content).selectAll('svg g.drops-container').attr('clip-path', '');
-  //d3.select(content).selectAll('svg g.drops-container').attr('transform', 'translate(-90, 0)');
+  // d3.select(content).selectAll('svg g.extremum').remove();
+  // d3.select(content).selectAll('svg').attr('width', 800);
+  // d3.select(content).selectAll('svg').attr('transform', 'translate(-90, 0)');
+   d3.select(content).selectAll('svg g.viewport').attr('transform', 'translate(-50, 20)');
+  // d3.select(content).selectAll('svg g.axes').attr('transform', 'translate(120, 55)');
+  // d3.select(content).selectAll('svg g.drops-container').attr('clip-path', '');
+  // d3.select(content).selectAll('svg g.drops-container').attr('transform', 'translate(-90, 0)');
 }
 
-window.onload=drawGraph;
-
+window.onload = drawGraph;
