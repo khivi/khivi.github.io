38dc03b16ef78400b8c0523bd50997da62f7f4a9
khivi <work@khivi.com>
2020-11-16 11:28:03 -0500

    Updated Website on Mon Nov 16 11:28:03 EST 2020

--- journey.js
+++ journey.js
@@ -1,191 +1,222 @@
-function drawGraph() {
-  // Themes begin
-  am4core.useTheme(am4themes_material);
-  am4core.useTheme(am4themes_animated);
-  // Themes end
+am4core.ready(function() {
 
-  var chart = am4core.create("chartdiv", am4plugins_timeline.SerpentineChart);
-  chart.curveContainer.padding(100, 20, 50, 20);
-  chart.levelCount = 3;
-  chart.yAxisRadius = am4core.percent(20);
-  chart.yAxisInnerRadius = am4core.percent(2);
-  chart.maskBullets = false;
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
-  var colorSet = new am4core.ColorSet();
-  colorSet.step = 5;
+var chart = am4core.create("chartdiv", am4plugins_timeline.SerpentineChart);
+chart.curveContainer.padding(50, 20, 50, 20);
+chart.levelCount = 4;
+chart.yAxisRadius = am4core.percent(25);
+chart.yAxisInnerRadius = am4core.percent(-25);
+chart.maskBullets = false;
 
-  chart.dateFormatter.inputDateFormat = "yyyy-MM";
-  chart.dateFormatter.dateFormat = "yyyy-MM";
+var colorSet = new am4core.ColorSet();
+//colorSet.saturation = 0.5;
+
+
+const today = (new Date()).toISOString().split('T')[0].slice(0,7);
+
 
   chart.data = [
   {
-      "category": "",
+      "category": "Storage",
       "start": "1996-06",
       "end": "2002-02",
-      "text": "Bell Labs\nHigh Performance Database System",
-      "textDisabled": false,
-      "color": colorSet.next(),
+      "name": "Bell Labs",
+      "color": colorSet.getIndex(5),
   },
   {
-      "category": "",
+      "category": "Telecom",
       "start": "2002-02",
       "end": "2005-07",
-      "text": "Xebeo\nNext-Gen Router",
-      "textDisabled": false,
-      "color": colorSet.next(),
+      "name": "Xebeo",
+      "color": colorSet.getIndex(7),
   },
   {
-      "category": "",
+      "category": "Telecom",
       "start": "2005-10",
       "end": "2008-10",
-      "text": "Roundbox\nMobile TV Broadcasting",
-      "textDisabled": false,
-      "color": colorSet.next(),
+      "name": "Roundbox",
+      "color": colorSet.getIndex(7),
   },
   {
-      "category": "",
+      "category": "FinTech",
       "start": "2009-01",
       "end": "2012-04",
-      "text": "Mesh Capital\nHigh-Frequency Trading",
-      "textDisabled": false,
-      "color": colorSet.next(),
+      "name": "Mesh Capital",
+      "color": colorSet.getIndex(9),
   },
   {
-      "category": "",
+      "category": "FinTech",
       "start": "2012-06",
       "end": "2013-12",
-      "text": "Konnect2\nTrading for Credit-Markets",
-      "textDisabled": false,
-      "color": colorSet.next(),
+      "name": "Konnect2",
+      "color": colorSet.getIndex(9),
   },
   {
-      "category": "",
+      "category": "Healthcare",
       "start": "2014-01",
       "end": "2016-01",
-      "text": "Betterpath\nHealthcare Data Access",
-      "textDisabled": false,
-      "color": colorSet.next(),
+      "name": "Betterpath",
+      "color": colorSet.getIndex(11),
   },
     /*
   {
-      "category": "",
+      "category": "Netcore",
       "start": "2016-02",
       "end": "2016-07",
-      "text": "Netcore\nConsulting: Product Culture",
-      "textDisabled": false,
+      "name": "Netcore",
       "color": colorSet.next(),
   },
   {
-      "category": "",
+      "category": "Emissary",
       "start": "2016-08",
       "end": "2017-02",
-      "text": "Emissary",
-      "textDisabled": false,
+      "name": "Emissary",
       "color": colorSet.next(),
   },
     */
   {
-      "category": "",
+      "category": "Retail",
       "start": "2017-02",
       "end": "2020-04",
-      "text": "Target\nE-commerce Search ",
-      "textDisabled": false,
-      "color": colorSet.next(),
+      "name": "Target",
+      "color": colorSet.getIndex(13),
+  },
+  {
+      "category": "Advisor",
+      "start": "2016-01",
+      "end": today,
+      "name": "Advisor",
+      "color": colorSet.getIndex(15),
   },
   ];
 
-  chart.fontSize = 10;
-  chart.tooltipContainer.fontSize = 10;
+chart.dateFormatter.dateFormat = "yyyy-MM";
+chart.dateFormatter.inputDateFormat = "yyyy-MM";
+chart.fontSize = 11;
 
-  var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-  categoryAxis.dataFields.category = "category";
-  categoryAxis.renderer.grid.template.disabled = true;
-  categoryAxis.renderer.labels.template.paddingRight = 25;
-  categoryAxis.renderer.minGridDistance = 10;
+var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "category";
+//categoryAxis.renderer.grid.template.disabled = true;
+categoryAxis.renderer.labels.template.paddingRight = 25;
+categoryAxis.renderer.labels.template.propertyFields.fill = "color";
+categoryAxis.renderer.labels.template.propertyFields.stroke = "color";
+categoryAxis.renderer.labels.template.strokeOpacity = 0;
+categoryAxis.renderer.minGridDistance = 10;
+categoryAxis.renderer.innerRadius = -60;
+categoryAxis.renderer.radius = 60;
 
-  var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-  dateAxis.renderer.minGridDistance = 70;
-  dateAxis.baseInterval = { count: 5, timeUnit: "year" };
-  //dateAxis.renderer.tooltipLocation = 0;
-  //dateAxis.hideTooltipWhileZooming = true;
-  dateAxis.cursorTooltipEnabled = false;
-  dateAxis.renderer.line.strokeDasharray = "1,4";
-  dateAxis.renderer.line.strokeOpacity = 0.5;
+var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.minGridDistance = 70;
+dateAxis.baseInterval = { count: 1, timeUnit: "month" };
+dateAxis.renderer.tooltipLocation = 0;
+dateAxis.startLocation = -0.5;
+dateAxis.renderer.line.strokeDasharray = "1,4";
+dateAxis.renderer.line.strokeOpacity = 0.6;
+dateAxis.tooltip.background.fillOpacity = 0.2;
+dateAxis.tooltip.background.cornerRadius = 5;
+dateAxis.tooltip.label.fill = new am4core.InterfaceColorSet().getFor("alternativeBackground");
+dateAxis.tooltip.label.paddingTop = 7;
 
-  //dateAxis.tooltip.background.fillOpacity = 0.2;
-  //dateAxis.tooltip.background.cornerRadius = 5;
-  //dateAxis.tooltip.label.fill = new am4core.InterfaceColorSet().getFor("alternativeBackground");
-  //dateAxis.tooltip.label.paddingTop = 7;
+var labelTemplate = dateAxis.renderer.labels.template;
+labelTemplate.verticalCenter = "middle";
+labelTemplate.fillOpacity = 0.7;
+labelTemplate.background.fill = new am4core.InterfaceColorSet().getFor("background");
+labelTemplate.background.fillOpacity = 1;
+labelTemplate.padding(7, 7, 7, 7);
+  
 
-  dateAxis.endLocation = 0.5;
-  dateAxis.startLocation = 0;
+var series = chart.series.push(new am4plugins_timeline.CurveColumnSeries());
+series.columns.template.height = am4core.percent(20);
+series.columns.template.tooltipText = "{name}[/]";
+series.dataFields.openDateX = "start";
+series.dataFields.dateX = "end";
+series.dataFields.categoryY = "category";
+series.columns.template.propertyFields.fill = "color"; 
+series.columns.template.propertyFields.stroke = "color";
+series.columns.template.strokeOpacity = 0;
 
-  var labelTemplate = dateAxis.renderer.labels.template;
-  labelTemplate.verticalCenter = "middle";
-  labelTemplate.fillOpacity = 0.4;
-  labelTemplate.background.fill = new am4core.InterfaceColorSet().getFor("background");
-  labelTemplate.background.fillOpacity = 1;
-  labelTemplate.padding(7, 7, 7, 7);
+/*
+var bullet = series.bullets.push(new am4charts.CircleBullet());
+bullet.circle.radius = 3;
+bullet.circle.strokeOpacity = 0;
+bullet.propertyFields.fill = "color";
+bullet.locationX = 0;
 
-  var series = chart.series.push(new am4plugins_timeline.CurveColumnSeries());
 
-  series.dataFields.openDateX = "start";
-  series.dataFields.dateX = "end";
-  series.dataFields.categoryY = "category";
-  series.baseAxis = categoryAxis;
+var bullet2 = series.bullets.push(new am4charts.CircleBullet());
+bullet2.circle.radius = 3;
+bullet2.circle.strokeOpacity = 0;
+bullet2.propertyFields.fill = "color";
+bullet2.locationX = 1;
+*/
 
-  series.columns.template.height = am4core.percent(15);
-  //series.columns.template.propertyFields.fill = "black";
-  //series.columns.template.propertyFields.stroke = "black";
-  series.columns.template.strokeOpacity = 0;
-  series.columns.template.fillOpacity = 0.6;
 
-  var imageBullet1 = series.bullets.push(new am4plugins_bullets.PinBullet());
-  imageBullet1.locationX = 1;
-  imageBullet1.propertyFields.stroke = "color";
-  imageBullet1.background.propertyFields.fill = "color";
-  imageBullet1.image = new am4core.Image();
-  imageBullet1.image.propertyFields.href = "icon";
-  imageBullet1.image.scale = 0.5;
-  imageBullet1.circle.radius = am4core.percent(100);
-  imageBullet1.dy = -5;
+/*
+var imageBullet1 = series.bullets.push(new am4plugins_bullets.PinBullet());
+imageBullet1.disabled = true;
+imageBullet1.propertyFields.disabled = "disabled1";
+imageBullet1.locationX = 1;
+imageBullet1.circle.radius = 20;
+imageBullet1.propertyFields.stroke = "color";
+imageBullet1.background.propertyFields.fill = "color";
+imageBullet1.image = new am4core.Image();
+imageBullet1.image.propertyFields.href = "image1";
 
-  var textBullet = series.bullets.push(new am4charts.LabelBullet());
-  textBullet.label.propertyFields.fill = "color";
-  textBullet.label.propertyFields.text = "text";
-  textBullet.disabled = true;
-  textBullet.propertyFields.disabled = "textDisabled";
-  textBullet.label.strokeOpacity = 0;
-  textBullet.locationX = 1;
-  textBullet.dy = - 100;
-  textBullet.label.textAlign = "middle";
+var imageBullet2 = series.bullets.push(new am4plugins_bullets.PinBullet());
+imageBullet2.disabled = true;
+imageBullet2.propertyFields.disabled = "disabled2";
+imageBullet2.locationX = 0;
+imageBullet2.circle.radius = 20;
+imageBullet2.propertyFields.stroke = "color";
+imageBullet2.background.propertyFields.fill = "color";
+imageBullet2.image = new am4core.Image();
+imageBullet2.image.propertyFields.href = "image2";
+*/
 
-  /*
-  chart.scrollbarX = new am4core.Scrollbar();
-  chart.scrollbarX.align = "center"
-  chart.scrollbarX.width = am4core.percent(75);
-  chart.scrollbarX.opacity = 0.5;
-  */
 
-  var cursor = new am4plugins_timeline.CurveCursor();
-  chart.cursor = cursor;
-  cursor.xAxis = dateAxis;
-  cursor.yAxis = categoryAxis;
-  cursor.lineY.disabled = true;
-  cursor.lineX.strokeDasharray = "1,4";
-  cursor.lineX.strokeOpacity = 1;
+var eventSeries = chart.series.push(new am4plugins_timeline.CurveLineSeries());
+eventSeries.dataFields.dateX = "eventDate";
+eventSeries.dataFields.categoryY = "category";
+eventSeries.data = [
+    { category: "Storage", eventDate: "1999-06", letter: "Award", description: "Bell Labs President's Silver Award" },
+    { category: "Retail", eventDate: "2019-09", letter: "Award", description: "Chief Data Officer Award" },
+  { category: "Advisor", eventDate: "2015-10", letter: "Author", description: "Culture Seeds: Process, People and Companies" },
+  { category: "FinTech", eventDate: "2009-01", letter: "CTO", description: "" },
+  //{ category: "Retail", eventDate: "2018-03", letter: "Director", description: "" },
+  { category: "Retail", eventDate: "2017-03", letter: "Sr. Director", description: "" },
+  { category: "Healthcare", eventDate: "2014-01", letter: "CTO", description: "" },
+  { category: "Telecom", eventDate: "2006-01", letter: "Manager", description: "" },
+  { category: "Telecom", eventDate: "2017-05", letter: "Patent", description: "Location specific event broadcasting " },
+  { category: "Telecom", eventDate: "2014-04", letter: "Patent", description: "Reliable event broadcaster with multiplexing and bandwidth control functions " },
+    ];
+eventSeries.strokeOpacity = 0;
 
-  dateAxis.renderer.tooltipLocation2 = 0;
-  categoryAxis.cursorTooltipEnabled = false;
 
-  /*
-  var label = chart.createChild(am4core.Label);
-  label.text = "Another unlucky day in the office."
-  label.isMeasured = false;
-  label.y = am4core.percent(40);
-  label.x = am4core.percent(50);
-  label.horizontalCenter = "middle";
-  label.fontSize = 20;
-  */
-}
-window.onload = drawGraph;
+var flagBullet = eventSeries.bullets.push(new am4plugins_bullets.FlagBullet())
+flagBullet.label.propertyFields.text = "letter";
+flagBullet.locationX = 0;
+flagBullet.tooltipText = "{description}";
+
+/*
+chart.scrollbarX = new am4core.Scrollbar();
+chart.scrollbarX.align = "center"
+chart.scrollbarX.width = am4core.percent(85);
+*/
+
+var cursor = new am4plugins_timeline.CurveCursor();
+chart.cursor = cursor;
+cursor.xAxis = dateAxis;
+cursor.yAxis = categoryAxis;
+cursor.lineY.disabled = true;
+cursor.lineX.strokeDasharray = "1,4";
+cursor.lineX.strokeOpacity = 1;
+
+dateAxis.renderer.tooltipLocation2 = 0;
+categoryAxis.cursorTooltipEnabled = false;
+dateAxis.cursorTooltipEnabled = false;
+
+
+});
